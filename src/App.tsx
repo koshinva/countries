c271c8a0f87c3808b8950bfb6c142631af751bf1
Koshin Valery <koshinva@yandex.ru>
2023-09-27 16:38:57 +0300

    feat: установить и настроить tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div>hello</div>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
-export default App
+export default App;
