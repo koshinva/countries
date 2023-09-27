02a414b8eafa87ff6936e45d7c2f3d4a7a87fb11
Koshin Valery <koshinva@yandex.ru>
2023-09-27 17:20:58 +0300

    feat: перейти на .scss, добавить темную тему в настройки tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      serif: ['Nunito Sans', 'sans-serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      'white': '#ffffff',
+      'elements-d': 'hsl(209, 23%, 22%)',
+      'bg-d': 'hsl(207, 26%, 17%)',
+      'text-d': 'hsl(0, 0%, 100%)',
+      'elements-l': 'hsl(0, 0%, 100%)',
+      'bg-l': 'hsl(0, 0%, 98%)',
+      'text-l': 'hsl(200, 15%, 8%)',
+      'input-l': 'hsl(0, 0%, 52%)'
+    }
   },
   plugins: [],
 }
-
